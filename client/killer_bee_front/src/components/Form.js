1af09c2808ed3746fb31e64db016bc6fffe9da86
lucasXIII <kem.lucas13@gmail.com>
2024-09-26 22:16:39 +0200

    App predone

---
+++ client/killer_bee_front/src/components/Form.js
@@ -0,0 +1,15 @@
+import { Select, TextField } from "@mui/material";
+
+
+
+export default function Form(){
+    return (
+        <>
+            <TextField variant="outlined"></TextField>
+            <TextField variant="outlined"></TextField>
+            <TextField variant="outlined"></TextField>
+            <TextField variant="outlined"></TextField>
+            <Select variant="outlined"></Select>
+        </>
+    )
+}
---
+++ client/killer_bee_front/src/components/Tabs.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import AddIcon from '@mui/icons-material/Add';
+
+export default function Tabs() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Freezbe Models" value="1" />
+            <Tab label="Ingredients" value="2" />
+            <Tab label="Procedures" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+            <Button variant='contained'><AddIcon/>Add</Button>
+        </TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
