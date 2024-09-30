046827473803f79c39ef54822f363752705e78b4
SOMA Chabane <schabane5@gmail.com>
2024-09-30 11:25:46 +0200

    Connexion with BDD etablished

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,14 +1,12 @@
-import { Sequelize } from 'sequelize';
+const sql = require('msnodesqlv8');
 
-const sequelize = new Sequelize('KillerBee', 'NEXTGEN\/2113136', '', {
-    host: 'localhost',
-    dialect: 'mssql',
-    dialectOptions: {
-        options: {
-            encrypt: true,
-            trustServerCertificate: true,
-        },
-    },
+const connectionString = "server=NR-CISCO-20\\MSSQLSERVER01;Database=KillerBee;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
+const query = "SELECT * FROM Utilisateur";
+
+sql.query(connectionString, query, (err, rows) => {
+    if (err) {
+        console.error("Erreur lors de l'exécution de la requête : ", err);
+        return;
+    }
+    console.log(rows);
 });
-
-export default sequelize;
