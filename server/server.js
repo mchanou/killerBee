fb8ecb7ee09439f24b4ff4ed7a4cd02af09caec4
SOMA Chabane <schabane5@gmail.com>
2024-09-30 14:49:07 +0200

    env file added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
- import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const routes = require('./routes'); // Allows to import all routes
 
 app.use(cors());
+app.use(express.json());
+
+TODO : "Create a index.js file in the route folder";
+app.use('/api', routes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,3 +1,5 @@
-const connectionString = "server=NR-CISCO-20\\MSSQLSERVER01;Database=KillerBee;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server};";
+require("dotenv").config({path:"../../.env"});
+
+const connectionString = process.env.DB_CONNECTION;
 
 module.exports = connectionString;
