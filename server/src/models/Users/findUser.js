67d426feccd8301d9b66df400dbde4332222ce23
SOMA Chabane <schabane5@gmail.com>
2024-10-01 14:52:27 +0200

    findUser part done but also need some modifications

--- server/src/models/Users/findUser.js
+++ server/src/models/Users/findUser.js
@@ -3,20 +3,25 @@ const express = require('express');
 const router = express.Router();
 const sql = require('msnodesqlv8');
 
+// Route pour rechercher un utilisateur par nom et prénom
+router.get('/:prenom/:nom', (req, res) => {
+    const prenomUser = req.params.prenom;
+    const nomUser = req.params.nom;
 
-// Route to find user by ID
-router.get('/:id', (req, res) => {
-    const userId = parseInt(req.params.id, 10);
-    const query = `SELECT * FROM Utilisateur WHERE Id = ?`;
+    const query = `SELECT * FROM Utilisateur WHERE PrenomUser = ? AND NomUser = ?`;
 
-    sql.query(connectionString, query, [userId], (err, rows) => {
+    sql.query(connectionString, query, [prenomUser, nomUser], (err, rows) => {
         if (err) {
             console.error("Erreur lors de la récupération de l'utilisateur : ", err);
             return res.status(500).json({ message: 'Erreur lors de la récupération de l\'utilisateur' });
         }
+
+        // Vérifier si aucun utilisateur n'a été trouvé
         if (rows.length === 0) {
             return res.status(404).json({ message: 'Utilisateur non trouvé' });
         }
+
+        // Retourner les détails de l'utilisateur trouvé
         res.json(rows[0]);
     });
 });
